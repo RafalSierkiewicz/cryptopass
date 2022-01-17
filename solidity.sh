4370fac9bf624994b91b08f440deacfb27ca45a6
Rafal Sierkiewicz <rafal.sierkiewicz@senti1.com>
2022-01-17 19:45:10 +0100

    Updated script path and added class generation

--- solidity.sh
+++ solidity.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-CONTRACT_PATH="cryptopass-web/src/main/resources/solidity"
+CONTRACT_PATH="contracts/password-manager"
 
 docker run -v $PWD/$CONTRACT_PATH:/sources ethereum/solc:stable -o /sources --abi --bin /sources/PasswordManager.sol
+sbt "web/runMain io.crypto.pass.services.blockchain.Generator"
